865307b948be058bf7ead91dcfedff46e93196f3
Noman Kabeer <nomankabeerr@gmail.com>
2020-07-17 14:47:08 +0500

    Edit post form validation

--- post/forms.py
+++ post/forms.py
@@ -6,3 +6,7 @@ class PostFormValidation(forms.Form):
     cover = forms.ImageField(required=True)
     gallery = forms.ImageField(required=True)
 
+
+class PostFormEditValidation(forms.Form):
+    title = forms.CharField(min_length=10  ,  max_length=255 , required=True)
+    content = forms.CharField(min_length=20 , max_length=5000 , required=True)
