a893fdb69823bad670f807bdf0dcfa0bb8b2bfca
Noman Kabeer <nomankabeerr@gmail.com>
2020-07-17 14:45:27 +0500

    post form validation

---
+++ post/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class PostFormValidation(forms.Form):
+    title = forms.CharField(min_length=10  ,  max_length=255 , required=True)
+    content = forms.CharField(min_length=20 , max_length=5000 , required=True)
+    cover = forms.ImageField(required=True)
+    gallery = forms.ImageField(required=True)
+
