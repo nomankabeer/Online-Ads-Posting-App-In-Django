8f672385743484b4f622c0abd4d20f1542034d7e
Noman Kabeer <nomankabeerr@gmail.com>
2020-07-20 16:17:19 +0500

    delete images on post update

--- post/models.py
+++ post/models.py
@@ -59,3 +59,23 @@ def auto_delete_file_on_delete_post(sender, instance, **kwargs):
         if os.path.isfile(instance.cover.path):
             os.remove(instance.cover.path)
 
+
+@receiver(models.signals.pre_save, sender=Posts)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """
+    Deletes old file from filesystem
+    when corresponding `MediaFile` object is updated
+    with new file.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = sender.objects.get(pk=instance.pk).cover
+    except sender.DoesNotExist:
+        return False
+
+    new_file = instance.cover
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
