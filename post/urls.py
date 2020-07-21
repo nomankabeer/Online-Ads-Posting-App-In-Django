eab1aab135cb8ea34a321c9ddf0c8045259616a3
Noman Kabeer <nomankabeerr@gmail.com>
2020-07-22 02:37:33 +0500

    Working on post listing

--- post/urls.py
+++ post/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = "post"
 urlpatterns = [
-    path('' , views.dashboard , name="dashboard")
+    path('' , views.dashboard , name="dashboard"),
+    path('post/index' , views.postIndex , name="index"),
 ]
 
--- post/views.py
+++ post/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Posts , Gallery as post_gallery
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import permission_required
 
 @login_required(redirect_field_name='next', login_url='/login')
 def dashboard(request):
     return  render(request , 'dashboard.html')
+
+@login_required(redirect_field_name='next', login_url='/login')
+@permission_required('post.view_posts', login_url='/login', raise_exception=False)
+def postIndex(request):
+    posts_all = Posts.objects.order_by('-id').filter(user_id=request.user.id)
+    paginator = Paginator(posts_all, 10) # Show 25 contacts per page.
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+    # posts = Posts.objects.order_by('-id').all()
+    context = {'posts': posts}
+    return  render(request , 'post/index.html' , context)
